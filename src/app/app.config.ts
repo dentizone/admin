75b8e96e9a9cb771ad9daec1f287fb82e7b1d6ca
Mahmoud Nasr <239.nasr@gmail.com>
2025-07-12 15:07:19 +0300

    fix(order): update currency display to use EGP format for better clarity fix(withdrawal-req): change currency display to EGP format for consistency refactor(app): register locale data for Arabic (Egypt) support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
+import { registerLocaleData } from '@angular/common';
+import localeEg from '@angular/common/locales/ar-EG';
+
+registerLocaleData(localeEg);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([AuthInterceptor])),
+    { provide: LOCALE_ID, useValue: 'ar-EG' }
   ],
 };
